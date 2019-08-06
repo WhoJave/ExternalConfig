5a418175394ed5aa961913b094ff03385a62f97e
Newman <newmanforios@gmail.com>
2019-08-06 18:00:54 +0800

    basic UI

---
+++ ExternalConfig/ExternalConfig/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ExternalConfig
+//
+//  Created by asd on 6/8/2019.
+//  Copyright © 2019 OnePiece. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
